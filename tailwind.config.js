45e65ee9e4a1989c26e0bd4df9517983730ba8f8
Rahul <yveltal4286@gmail.com>
2024-07-26 21:35:25 +0530

    :heavy_plus_sign: & :art: npm update tailwind , bg-images, new grids

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const { white } = require("color-name");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,11 +6,6 @@ module.exports = {
     "./public/index.html",
   ],
   theme: {
-    colors: {
-      gray: "#0d1717",
-      white: "#f0f0f0",
-      black: "#000000",
-    },
     extend: {},
   },
   plugins: [require("daisyui")],
